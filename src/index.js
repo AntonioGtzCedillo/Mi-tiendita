853774efce50272ddb2be884ae2ec9434617d61b
Jose Antonio <antonio.gutierrezc@hotmail.com>
2022-12-07 15:11:51 -0600

    modificacion de css y html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css"
+
+const nosotros = document.querySelector('.nosotros');
+
+nosotros.addEventListener("click", () =>{
+    console.log("me diste click")
+})
+
+// const ddpNosotros = document.createElement("p")
+// ddpNosotros.classList.add("info-nosotros")
+// document.querySelector(".busqueda").style.display = "none"
